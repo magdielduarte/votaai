e6229e6cd5013f76d879cabcf3a1de05fada3561
Magdiel <magdiel.kesller@gmail.com>
2014-09-28 16:45:23 -0300

    linkando menu iniciar votacao com section votacao

--- Layout/votacao/dev/assets/js/main.js
+++ Layout/votacao/dev/assets/js/main.js
@@ -1,7 +1,20 @@
+
+/*
+	Apaga as setas do fullpage js, pois o mesmo não existe opção default para tal
+*/
 setTimeout(function(){
 	var arrowsfb = document.getElementsByClassName('fp-controlArrow');
 	for (var i = 0; i < arrowsfb.length; i++) {
 		arrowsfb[i].style.display = 'none';
 	}
 },10);
+
+
+/*
+	link para começar a votação
+*/
  
+var linkVotar = document.getElementById('linkVotar');
+linkVotar.onclick = function() {
+	$.fn.fullpage.moveTo(2);
+};
