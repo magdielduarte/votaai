5220d15bdd47516e2eb151bec1599335bef8877d
tuliomamute <tulio.mamute@hotmail.com>
2014-10-29 12:38:26 -0200

    troca de ordem dos botões
    troca de ordem dos botões

--- Votaai/Votaai/UserControl/Partido.ascx.designer.cs
+++ Votaai/Votaai/UserControl/Partido.ascx.designer.cs
@@ -85,15 +85,6 @@ namespace Votaai.UserControl {
         protected global::System.Web.UI.WebControls.TextBox prefixopartido;
         
         /// <summary>
-        /// BtnCadPart control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button BtnCadPart;
-        
-        /// <summary>
         /// BtnCanPart control.
         /// </summary>
         /// <remarks>
@@ -101,5 +92,14 @@ namespace Votaai.UserControl {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button BtnCanPart;
+        
+        /// <summary>
+        /// BtnCadPart control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnCadPart;
     }
 }
